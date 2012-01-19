ead05da007442be97ea4bf4fbfe31f723c048db5
Florian Kaufmann <sensorflo@gmail.com>
2012-01-19 23:22:55 +0100

    split up the current big test into smaller ones

--- adoc-mode-test.el
+++ adoc-mode-test.el
@@ -3,8 +3,15 @@
 
 ;; todo
 ;; test for font lock multiline property 
+;; todo
+;;  !!!!!!!!!!!!!
+;;  Lock Support Mode must be set to nil
+;;  !!!!!!!!!!!!!
 
-(defun adoctest-make-buffer (&rest args)
+;; todo: test for presence of adoc-reserved (we do white-box testing here)
+
+
+(defun adoctest-faces (&rest args)
   (set-buffer (get-buffer-create "adoctest")) 
   (delete-region (point-min) (point-max))
 
@@ -13,19 +20,22 @@
     (setq args (cddr args)))
 
   (adoc-mode)
-  (font-lock-fontify-buffer))
+  (font-lock-fontify-buffer)
+  (goto-char (point-min))
+  (let ((not-done t))
+    (while not-done
+      (let* ((tmp (get-text-property (point) 'adoctest))
+	     (tmp2 (get-text-property (point) 'face)))
+	(when tmp
+	  (if (and (listp tmp2) (not (listp tmp)))
+	      (ert-should (and (= 1 (length tmp2)) (equal tmp (car tmp2))))
+	    (ert-should (equal tmp tmp2))))
+	(if (< (point) (point-max))
+	    (forward-char 1)
+	  (setq not-done nil))))))
 
-(ert-deftest adoctest-foo ()
-  ;; todo
-  ;;  !!!!!!!!!!!!!
-  ;;  Lock Support Mode must be set to nil
-  ;;  !!!!!!!!!!!!!
-
-  ;; todo: test for presence of adoc-reserved (we do white-box testing here)
-  
-
-  ;; the white after = really is part of the delimiter
-  (adoctest-make-buffer
+(ert-deftest adoctest-test-titles-simple ()
+  (adoctest-faces
    "= " markup-meta-hide-face "document title" markup-title-0-face "\n" nil
    "\n" nil
    "== " markup-meta-hide-face "chapter 1" markup-title-1-face "\n" nil
@@ -63,9 +73,10 @@
    "^^^^^^^^^" markup-meta-hide-face "\n" nil
    "\n" nil
    "chapter 4" markup-title-4-face "\n" nil
-   "+++++++++" markup-meta-hide-face "\n" nil
-   "\n" nil
+   "+++++++++" markup-meta-hide-face "\n" nil))
 
+(ert-deftest adoctest-test-delimited-blocks-simple ()
+  (adoctest-faces
    "////////" markup-meta-hide-face "\n" nil
    "comment line 1\ncomment line 2" markup-comment-face "\n" nil
    "////////" markup-meta-hide-face "\n" nil
@@ -96,9 +107,10 @@
    "\n" nil
    "--" markup-meta-hide-face "\n" nil
    "open block line 1\nopen block line 2" nil "\n" nil
-   "--" markup-meta-hide-face "\n" nil
-   "\n" nil
+   "--" markup-meta-hide-face "\n" nil))
 
+(ert-deftest adoctest-test-quotes-simple ()
+  (adoctest-faces
    ;; note that in unconstraned quotes cases " ipsum " has spaces around, in
    ;; constrained quotes case it doesn't
    "Lorem " nil "`" markup-meta-hide-face "ipsum" '(markup-typewriter-face markup-verbatim-face) "`" markup-meta-hide-face " dolor\n" nil
@@ -116,21 +128,7 @@
    "Lorem " nil "##" markup-meta-hide-face " ipsum " markup-gen-face "##" markup-meta-hide-face " dolor\n" nil
    "Lorem " nil "#" markup-meta-hide-face "ipsum" markup-gen-face "#" markup-meta-hide-face " dolor\n" nil
    "Lorem " nil "~" markup-meta-hide-face " ipsum " markup-subscript-face "~" markup-meta-hide-face " dolor\n" nil
-   "Lorem " nil "^" markup-meta-hide-face " ipsum " markup-superscript-face "^" markup-meta-hide-face " dolor\n" nil
-   )
+   "Lorem " nil "^" markup-meta-hide-face " ipsum " markup-superscript-face "^" markup-meta-hide-face " dolor\n" nil))
 
-  (goto-char (point-min))
-  (let ((not-done t))
-    (while not-done
-      (let* ((tmp (get-text-property (point) 'adoctest))
-	     (tmp2 (get-text-property (point) 'face)))
-	(when tmp
-	  (if (and (listp tmp2) (not (listp tmp)))
-	      (ert-should (and (= 1 (length tmp2)) (equal tmp (car tmp2))))
-	    (ert-should (equal tmp tmp2))))
-	(if (< (point) (point-max))
-	    (forward-char 1)
-	  (setq not-done nil))))))
-
-(ert-run-tests-interactively "^adoctest-")
+(ert-run-tests-interactively "^adoctest-test-")
 
