0a35871ac3a2ea49ea103cbe8c518866217e7787
Florian Kaufmann <sensorflo@gmail.com>
2013-01-17 11:49:08 +0100

    edited (doc-)comments to make it fit package format guidelines

--- adoc-mode.el
+++ adoc-mode.el
@@ -3,10 +3,10 @@
 ;; Copyright 2010-2013 Florian Kaufmann <sensorflo@gmail.com>
 ;;
 ;; Author: Florian Kaufmann <sensorflo@gmail.com>
-;; URL: Current official: http://code.google.com/p/adoc-mode/
-;;      Develop, future official: https://github.com/sensorflo/adoc-mode
+;; URL: https://github.com/sensorflo/adoc-mode/wiki
 ;; Created: 2009
 ;; Version: 0.6.0
+;; Package-Requires: ((markup-faces "1.0.0"))
 ;; Keywords: wp AsciiDoc
 ;; 
 ;; This file is not part of GNU Emacs.
